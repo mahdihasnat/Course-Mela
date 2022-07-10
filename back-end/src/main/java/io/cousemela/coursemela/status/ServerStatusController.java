9f3ce38eaf785f70d578990794e1c79bf31b6c06
vodro <vdrkowshic@gmail.com>
2022-07-10 18:09:30 +0600

    server time added

--- back-end/src/main/java/io/cousemela/coursemela/status/ServerStatusController.java
+++ back-end/src/main/java/io/cousemela/coursemela/status/ServerStatusController.java
@@ -4,6 +4,10 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class ServerStatusController {
@@ -11,6 +15,8 @@ public class ServerStatusController {
 
     @RequestMapping("/status")
     public String getServerStatus() {
-        return "Server is up and running";
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        return "Server is up and running @" + dateFormat.format(date) ;
     }
 }
