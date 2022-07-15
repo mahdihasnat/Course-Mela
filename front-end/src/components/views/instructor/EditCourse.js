575bf722e660e6f29f600691ddef81c18ced8a1d
rh2975 <75852496+rh2975@users.noreply.github.com>
2022-07-16 00:52:02 +0600

    Video + Player added

--- front-end/src/components/views/instructor/EditCourse.js
+++ front-end/src/components/views/instructor/EditCourse.js
@@ -57,12 +57,20 @@ const nColsHalved = Math.floor(nCols/2);
 function EditCourse() {
 
   const [selectedImg, setSelectedImg] = useState(null);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
   const [addQuestions, setAddQuestions] = useState(false);
   
   const handleImgUpload = e => {
     if(e.target.files.length !== 0) {
         setSelectedImg(e.target.files[0]);
+    }
+  }
+
+  const handleVideoUpload = e => {
+    if(e.target.files.length !== 0) {
+        setSelectedVideo(e.target.files[0]);
+        console.log(e.target.files[0].name);
     }
   }
 
@@ -100,7 +108,7 @@ function EditCourse() {
                 }
             </ul>
         </div>
-        <div className='container' style={{ backgroundColor: "rgb(255, 244, 118)", padding: "20px", borderRadius: "20px", marginTop: "10vh" }}>
+        <div className='container' style={{ position: "relative", backgroundColor: "rgb(255, 244, 118)", padding: "20px", borderRadius: "20px", marginTop: "10vh" }}>
             <div style={{ fontSize: "1.5rem", fontWeight: "bold", marginBottom: "20px" }}>
                 Add New Topic
             </div>
@@ -119,9 +127,23 @@ function EditCourse() {
                             }
                         </span>
                         <span className='upload-courseimg-label'>
-                            <span><i className="fa fa-camera" style={{ color: "white", fontSize: "15px", marginRight: "10px" }}></i>Upload TopicThumb</span> 
+                            <span><i className="fa fa-camera" style={{ color: "white", fontSize: "15px", marginRight: "10px" }}></i>Upload Thumbnail</span> 
                         </span>
                         <input id='upload-courseimg' type='file' onChange={handleImgUpload} accept="image/png, image/jpg, image/jpeg, image/bmp" />
+                    </label>
+                    <label htmlFor='upload-coursevideo' className='' style={{ display: "flex", flexDirection: "column" }}>
+                        <span style={{ marginTop: "20px" }}>
+                            { !selectedVideo? <>No file chosen</> : 
+                            <video width={"250px"} height={"250px"} controls>
+                                <source src={URL.createObjectURL(selectedVideo)} type="video/mp4" />
+                            </video>
+                                // <img alt='not found' width={"250px"} src={URL.createObjectURL(selectedVideo)} /> 
+                            }
+                        </span>
+                        <span className='upload-courseimg-label'>
+                            <span><i className="fa fa-video-camera" style={{ color: "white", fontSize: "15px", marginRight: "10px" }}></i>Upload Video</span> 
+                        </span>
+                        <input id='upload-coursevideo' type='file' onChange={handleVideoUpload} accept="video/mp4" />
                     </label>
                 </div>
                 <div>
@@ -139,6 +161,7 @@ function EditCourse() {
                     </form>
                 </div>
                 <div>
+                    {/* <input type={"submit"} className='upload-courseimg-label' value="Update" />  */}
                     <span className='upload-courseimg-label'>
                         <span>Update</span> 
                     </span>
