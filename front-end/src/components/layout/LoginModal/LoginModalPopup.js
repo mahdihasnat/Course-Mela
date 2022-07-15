5311eee9f38189dcc59f0ec16ef81ccba12e08d7
rh2975 <75852496+rh2975@users.noreply.github.com>
2022-07-15 23:50:55 +0600

    Update LoginModalPopup.js

--- front-end/src/components/layout/LoginModal/LoginModalPopup.js
+++ front-end/src/components/layout/LoginModal/LoginModalPopup.js
@@ -8,6 +8,8 @@ import { LoginWithDispatch } from '../../../store/storeIndex';
 const LoginModalPopup = () => {
 
   const [state, dispatch] = useLoginContext();
+  const [username, setUsername] = useState("");
+  const [pwd, setPwd] = useState("");
 
   const handleCloseModal = () => {
     dispatch({ type: "LOGIN_MODAL_CANCELED" });
@@ -27,7 +29,7 @@ const LoginModalPopup = () => {
     //   console.log(error);
     // }
     // dispatch({ type: "LOGIN" })
-    LoginWithDispatch("jhon", "123", dispatch)
+    LoginWithDispatch(username, pwd, dispatch)
     // dispatch(Login("jhon", "123"));	
     console.log("Login clicked ");
   }
@@ -41,10 +43,10 @@ const LoginModalPopup = () => {
             <div className="login-container">
                 <div style={{ display: "flex", alignItems: "center", justifyContent: "center", fontSize: "1.1rem", fontWeight: "bold" }}>LOGIN</div>
                 <label><b>Username</b></label>
-                <input type="text" placeholder="Enter Username" name="uname" />
+                <input type="text" placeholder="Enter Username" name="uname" onChange={e => setUsername(e.target.value)} />
     
                 <label><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" />
+                <input type="password" placeholder="Enter Password" name="psw" onChange={e => setPwd(e.target.value)} />
     
                 <input type="submit" className="popup-btn-login" value="Login" style={{background: "none", border: "none", backgroundColor: "#ddd", marginTop: "8px", padding: "11px", borderRadius: "10px", width: "100%" }} />
             </div>
