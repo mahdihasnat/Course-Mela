e61cd8255472492e4672d1abd4b2823407910ae5
rh2975 <75852496+rh2975@users.noreply.github.com>
2022-07-17 04:11:34 +0600

    Video upload + popup timer acc. to duration

--- front-end/src/components/views/instructor/AddNewVideo.js
+++ front-end/src/components/views/instructor/AddNewVideo.js
@@ -1,35 +1,77 @@
 import React, { useState } from 'react'
+import FormatSeconds from '../../helper/FormatSeconds';
 
-function AddNewTopic() {
+function AddNewVideo() {
 
     const [selectedImg, setSelectedImg] = useState(null);
     const [selectedVideo, setSelectedVideo] = useState(null);
 
     const [addQuestions, setAddQuestions] = useState(false);  
+    const [errorOnAddQuestion, setErrorOnAddQuestion] = useState("");
+
+    const [videoLengthInSec, setVideoLengthInSec] = useState(420);
+    const [questionToBeAddedAt, setQuestionToBeAddedAt] = useState(0);
+
+    const vidDuration = React.useRef(null);
 
     const handleImgUpload = e => {
         if(e.target.files.length !== 0) {
             setSelectedImg(e.target.files[0]);
         }
       }
-    
+
       const handleVideoUpload = e => {
-        if(e.target.files.length !== 0) {
-            setSelectedVideo(e.target.files[0]);
-            console.log(e.target.files[0].name);
+        // if(e.target.files.length !== 0) {
+        //     setSelectedVideo(e.target.files[0]);
+        //     console.log(e.target.files[0].name);
+            
+        //     setErrorOnAddQuestion("");
+        //     console.log("VidDuration = ", videoLengthInSec);
+        // }
+        
+        e.preventDefault();
+        var reader = new FileReader();
+        reader.onloadend = () => {
+            setSelectedVideo(reader.result);
+            // console.log(reader);
+            setErrorOnAddQuestion("");
+        };
+        reader.readAsDataURL(e.target.files[0]);
+
+        // var media = new Audio(reader.result);
+        // media.onloadedmetadata = () => {
+        //     console.log("VIdDuration = ", media.duration);
+        // }
+      }
+
+      const handleQuestionAddClick = () => {
+        if(selectedVideo) {
+            setAddQuestions(true);
+            
+
+            var media = new Audio(selectedVideo);
+            media.onloadedmetadata = () => {
+                console.log("VIdDuration = ", media.duration);
+                setVideoLengthInSec(media.duration);
+            }
+        }
+        else {
+            setAddQuestions(false);
+            setErrorOnAddQuestion("Add a video first");
         }
       }
     
       const handleQuestionUpload = e => {
         e.preventDefault();
-        console.log("AJHSJA");
+        console.log("Questions uploaded");
       }
 
   return (
     <div className='container' style={{ position: "relative", backgroundColor: "rgb(255, 244, 118)", padding: "20px", borderRadius: "20px", marginTop: "10vh" }}>
         <div style={{ fontSize: "1.5rem", fontWeight: "bold", marginBottom: "20px" }}>
-            Add New Topic
+            Add New Video
         </div>
+        <form onSubmit={handleQuestionUpload} id='quesion_upload_form'></form>
         <form style={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}>
             <div>
                 <label htmlFor='topic-title'>Title</label>
@@ -52,9 +94,10 @@ function AddNewTopic() {
                 <label htmlFor='upload-coursevideo' className='' style={{ display: "flex", flexDirection: "column" }}>
                     <span style={{ marginTop: "20px" }}>
                         { !selectedVideo? <>No file chosen</> : 
-                        <video width={"250px"} height={"250px"} controls>
+                        /*<video ref={vidDuration} width={"250px"} height={"250px"} controls>
                             <source src={URL.createObjectURL(selectedVideo)} type="video/mp4" />
-                        </video>
+                        </video>*/
+                        <video width={"250px"} height={"250px"} src={selectedVideo} controls></video>
                             // <img alt='not found' width={"250px"} src={URL.createObjectURL(selectedVideo)} /> 
                         }
                     </span>
@@ -65,18 +108,21 @@ function AddNewTopic() {
                 </label>
             </div>
             <div>
-                Questions<i className="fa fa-plus" onClick={() => setAddQuestions(true)} style={{ color: "black", fontSize: "15px", marginLeft: "10px", backgroundColor: "#bbb", paddingLeft: "5px", paddingRight: "5px", paddingTop: "3px", paddingBottom: "3px", borderRadius: "50px", cursor: "pointer" }}></i>
-                <form onSubmit={handleQuestionUpload}>
+                Questions<i className="fa fa-plus" onClick={handleQuestionAddClick} style={{ color: "black", fontSize: "15px", marginLeft: "10px", backgroundColor: "#bbb", paddingLeft: "5px", paddingRight: "5px", paddingTop: "3px", paddingBottom: "3px", borderRadius: "50px", cursor: "pointer" }}></i>
+                <div style={ errorOnAddQuestion? {display: "block", marginTop: "10px", color: "red" } : {display: "none"} }>
+                    { errorOnAddQuestion }
+                </div>
                 {
                     addQuestions && (
                         <div>
-                            <input type={"text"} />
-                            <input type={"submit"} value='Add' />
+                            <input type={"text"} form='quesion_upload_form' />
+                            At time: (<FormatSeconds seconds={videoLengthInSec * questionToBeAddedAt / 100.0} />)<br />
+                            <FormatSeconds seconds={0} /><input type={"range"} value={questionToBeAddedAt} form='question_upload_form' onChange={(e) => setQuestionToBeAddedAt(e.target.value)} /><FormatSeconds seconds={videoLengthInSec} />
+                            <br /><br />
+                            <input type={"submit"} value='Add' form='quesion_upload_form' />
                         </div>
                     )
                 }
-                
-                </form>
             </div>
             <div>
                 {/* <input type={"submit"} className='upload-courseimg-label' value="Update" />  */}
@@ -92,4 +138,4 @@ function AddNewTopic() {
   )
 }
 
-export default AddNewTopic
+export default AddNewVideo
