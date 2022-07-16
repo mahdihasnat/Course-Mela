623f6b0e9dca3958c3cbc3434c3d09b0c8d2d62d
vodro <vdrkowshic@gmail.com>
2022-07-16 19:58:30 +0600

    getting errors in both endpoints

--- front-end/src/shared/Header.js
+++ front-end/src/shared/Header.js
@@ -4,3 +4,8 @@ export const getHeader =  {
     'Authorization': 'Bearer ' + localStorage.getItem('jwtToken'),
         
 };
+
+export const jsonAuthorizedHeader = {  
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + localStorage.getItem('jwtToken'),
+};
