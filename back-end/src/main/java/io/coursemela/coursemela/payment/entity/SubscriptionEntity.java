2e0c307cc4c83f2ac3bf94a7f5b37e8be17e4349
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-10 23:35:48 +0600

    added subscription

---
+++ back-end/src/main/java/io/coursemela/coursemela/payment/entity/SubscriptionEntity.java
@@ -0,0 +1,46 @@
+package io.coursemela.coursemela.payment.entity;
+
+import io.coursemela.coursemela.course.entity.CoursePricingEntity;
+import io.coursemela.coursemela.student.entity.StudentEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import java.time.ZonedDateTime;
+
+
+@Embeddable
+class SubscriptionKey {
+    @Column(name = "studentId")
+    Long studentId;
+
+    @Column(name = "coursePricingId")
+    Long coursePricingId;
+
+}
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class SubscriptionEntity extends TransactionEntity {
+
+    @ManyToOne
+    private StudentEntity studentEntity;
+
+    @ManyToOne
+    private CoursePricingEntity coursePricingEntity;
+
+    @ManyToOne
+    private PromoEntity promoEntity;
+
+    private ZonedDateTime startTime;
+    private ZonedDateTime endTime;
+
+}
