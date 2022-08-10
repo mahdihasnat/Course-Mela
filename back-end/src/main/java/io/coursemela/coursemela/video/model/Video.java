406e8cbe1c43aa68106fae8e510318efff4f1416
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-11 00:59:59 +0600

    added video model

--- back-end/src/main/java/io/coursemela/coursemela/video/model/Video.java
+++ back-end/src/main/java/io/coursemela/coursemela/video/model/Video.java
@@ -1,17 +1,17 @@
 package io.coursemela.coursemela.video.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Getter;
+import lombok.Data;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
-@Getter
+@Data
 public class Video implements Serializable {
-    private final Long id;
-    private final String title;
-    private final String description;
-    private final Long likeCount;
-    private final Integer serial;
-    private final Boolean hidden;
+    private Long id;
+    private String title;
+    private String description;
+    private Long likeCount;
+    private Integer serial;
+    private Boolean hidden;
 }
