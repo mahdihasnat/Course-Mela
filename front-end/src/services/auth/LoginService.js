a9a76fed7d19e9d7d0cbf4ac823cb62235ff496e
vodro <vdrkowshic@gmail.com>
2022-08-12 19:04:00 +0600

    userRole was an array

--- front-end/src/services/auth/LoginService.js
+++ front-end/src/services/auth/LoginService.js
@@ -1,62 +1,64 @@
 import axios from "axios";
 import { jsonAuthorizedHeader } from "../../shared/Header";
 
-import {loginSuccess, loginError, loginCancel, loginSubmit} from './../../store/auth/AuthActions';
-import joinUrl from '../../utils/url';
-import {AUTH_URL} from '../../shared/urls';
+import {
+  loginSuccess,
+  loginError,
+  loginCancel,
+  loginSubmit,
+} from "./../../store/auth/AuthActions";
+import joinUrl from "../../utils/url";
+import { AUTH_URL } from "../../shared/urls";
 
-class LoginService{
-    
-    getRole(){
-        // alert(jsonAuthorizedHeader().Authorization)
+class LoginService {
+  getRole() {
+    // alert(jsonAuthorizedHeader().Authorization)
 
-        return axios({
-            method: "post",
-            url: joinUrl(AUTH_URL, "role"),
-            headers: jsonAuthorizedHeader(),
-        })
-    }
-    
-    
-    LoginWithDispatch(userName, password, dispatch){
+    return axios({
+      method: "post",
+      url: joinUrl(AUTH_URL, "role"),
+      headers: jsonAuthorizedHeader(),
+    });
+  }
 
-        dispatch(loginSubmit());
-          
-            axios.post(joinUrl(AUTH_URL,''), {
-                "userName": userName,
-                "password": password,
-            }) 
-                .then(response => {
-                    // console.log(response);
-                    // console.log(response.data.jwtToken);
-                    localStorage.setItem('jwtToken', response.data.jwtToken);
-                    // alert(localStorage.getItem('jwtToken'));
-                    // dispatch(loginCancel());
-                    this.getRole().then(response => {
+  LoginWithDispatch(userName, password, dispatch) {
+    dispatch(loginSubmit());
 
-                        console.log(response.data); // only role is returned
-                        const role = response.data.map(role => role.authority);
-                        console.log(role);
-                        localStorage.setItem('userRole', role);
-                        localStorage.setItem('userName', userName);
-                        dispatch(loginSuccess(role, userName));
-                        // return role;
-                        /// TO DO which role you select in the dropdown menu
-                    
-                    }).catch(error => {
-                        dispatch(loginError(error.message))
-                        console.log(error);
-                        return null;
-                    })
-                    
-    
-                }).catch(error => {
-                    console.log(error);
-                    dispatch(loginError(error.message));
-                }
-            );
-    
-    }
+    axios
+      .post(joinUrl(AUTH_URL, ""), {
+        userName: userName,
+        password: password,
+      })
+      .then((response) => {
+        // console.log(response);
+        // console.log(response.data.jwtToken);
+        localStorage.setItem("jwtToken", response.data.jwtToken);
+        // alert(localStorage.getItem('jwtToken'));
+        // dispatch(loginCancel());
+        this.getRole()
+          .then((response) => {
+            console.log(response.data); // only role is returned
+            const role = response.data.map((role) => role.authority);
+            const currentRole = role[0];
+            console.log(currentRole);
+            // alert({ role });
+            localStorage.setItem("userRole", currentRole);
+            localStorage.setItem("userName", userName);
+            dispatch(loginSuccess(currentRole, userName));
+            // return role;
+            /// TO DO which role you select in the dropdown menu
+          })
+          .catch((error) => {
+            dispatch(loginError(error.message));
+            console.log(error);
+            return null;
+          });
+      })
+      .catch((error) => {
+        console.log(error);
+        dispatch(loginError(error.message));
+      });
+  }
 }
 
 export default new LoginService();
