f350293fc29fe23e692bae6698280184956da533
vodro <vdrkowshic@gmail.com>
2022-08-21 23:10:14 +0600

    video watch comment working on

--- front-end/src/components/views/shared/videoWatch/VideoWatch.js
+++ front-end/src/components/views/shared/videoWatch/VideoWatch.js
@@ -20,38 +20,11 @@ import VideoService from "../../../../services/video/VideoService";
 import { LOG_CAUGHT_ERR } from "../../../../shared/utils";
 import Playlist from "./Playlist";
 
-const videoPlayerStyle = {
-  // bgcolor: "primary.main",
-  height: {
-    xs: 400,
-    md: 500,
-    xl: 700,
-  },
-  width: {
-    xs: 500,
-    md: 900,
-    xl: 1200,
-  },
-};
-
 const VideoWatch = ({}) => {
   const comments = [
     { commentId: "2", commenter: "Abul", comment: "What is sign used for" },
     { commentId: "1", commenter: "Amir", comment: "What is sign used? " },
   ];
-
-  // const playlists = [
-  //   { id: 1, thumbpath: require('../../../../assets/coursethumb1.png'), title: "Differentiation", description: "Limits and Conjugates", duration: "07:00", rating: 4.5 },
-  //   { id: 2, thumbpath: require('../../../../assets/coursethumb3.png'), title: "Integration", description: "Integration By Parts", duration: "03:00", rating: 5 },
-  //   { id: 3, thumbpath: require('../../../../assets/coursethumb1.png'), title: "Differentiation", description: "Limits and Conjugates", duration: "07:00", rating: 4.5 },
-  //   { id: 4, thumbpath: require('../../../../assets/coursethumb3.png'), title: "Integration", description: "Integration By Parts", duration: "03:00", rating: 5 },
-  //   { id: 5, thumbpath: require('../../../../assets/coursethumb1.png'), title: "Differentiation", description: "Limits and Conjugates", duration: "07:00", rating: 4.5 },
-  //   { id: 6, thumbpath: require('../../../../assets/coursethumb3.png'), title: "Integration", description: "Integration By Parts", duration: "03:00", rating: 5 },
-  //   { id: 7, thumbpath: require('../../../../assets/coursethumb1.png'), title: "Differentiation", description: "Limits and Conjugates", duration: "07:00", rating: 4.5 },
-  //   { id: 8, thumbpath: require('../../../../assets/coursethumb3.png'), title: "Integration", description: "Integration By Parts", duration: "03:00", rating: 5 },
-  //   { id: 9, thumbpath: require('../../../../assets/coursethumb1.png'), title: "Differentiation", description: "Limits and Conjugates", duration: "07:00", rating: 4.5 },
-  //   { id: 0, thumbpath: require('../../../../assets/coursethumb3.png'), title: "Integration", description: "Integration By Parts", duration: "03:00", rating: 5 },
-  // ]
 
   const { videoId } = useParams();
 
@@ -61,9 +34,8 @@ const VideoWatch = ({}) => {
 
   const [isLoading, setIsLoading] = useState(true);
 
-
   useEffect(() => {
-  /// loading the videos here 
+    /// loading the videos here
 
     VideoService.getVideoById(videoId)
       .then((response) => {
@@ -72,95 +44,110 @@ const VideoWatch = ({}) => {
         setIsLoading(false);
       })
       .catch(LOG_CAUGHT_ERR);
-  /// similar videos will be loaded here
-    
-    VideoService.getSimilarVideos(videoId).then((response) => {
-      console.log({
-        currentVideos: videoId,
-        similarVideos: response.data,
+    /// similar videos will be loaded here
 
+    VideoService.getSimilarVideos(videoId)
+      .then((response) => {
+        console.log({
+          currentVideos: videoId,
+          similarVideos: response.data,
+        });
+
+        setPlaylists(response.data);
       })
-
-      setPlaylists(response.data);
-
-    }).catch(LOG_CAUGHT_ERR);
-
+      .catch(LOG_CAUGHT_ERR);
   }, [videoId]);
 
   return (
-    <Box>
+    <>
       {isLoading ? (
         <CircularProgress height="100%" />
       ) : (
-        <Stack>
-          <Stack direction={"row"} sx={{ marginBottom: 2 }}>
-            <Box sx={videoPlayerStyle}>
-              <ReactPlayer
-                width={"100%"}
-                height={"100%"}
-                url={video.videoPath}
-                controls
-              ></ReactPlayer>
-            </Box>
-            <Box sx={{ height: 700, width: 450, overflow: "auto" }}>
+        <Grid container my={5}>
+          <Grid item sm={12} md={8} px={5}>
+            <ReactPlayer
+              width={"100%"}
+              height={"100%"}
+              url={video.videoPath}
+              controls
+            ></ReactPlayer>
+          </Grid>
+          <Grid item sm={12} md={4}>
+            <Stack spacing={2}>
+              {/* <Box sx={{ height: 700, width: 450, overflow: "auto" }}>
+                { */}
               {
-                /// TODO : check if it actually works 
-                playlists &&  playlists.map(playlist => <Playlist key={playlist.id} playlist={playlist} /> )
+                /// TODO : check if it actually works playlists &&
+                playlists.map((playlist) => (
+                  <Playlist key={playlist.id} playlist={playlist} />
+                ))
               }
-            </Box>
-          </Stack>
-          <Stack sx={{ margin: 5 }}>
-            <Box sx={{ bgcolor: "rgba(160, 111, 205, 0.7)" }}>
-              <Typography variant="h4" color="textPrimary" sx={{ padding: 2 }}>
-                Comments
+            </Stack>
+          </Grid>
+          <Grid item sm={12} md={8}>
+            <Stack spacing={2}>
+              <Typography variant="h4" color="textPrimary" align={"center"}>
+                Doubts people had...
               </Typography>
-              <Grid container spacing={3} sx={{ padding: 2 }}>
-                {comments.map((comment) => (
-                  <Grid item xs={12}>
-                    <Card>
-                      <CardHeader
-                        title={comment.commenter}
-                        subheader={comment.comment}
-                      />
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-              <Box sx={{  padding: 2 }}>
-                <form
-                  onSubmit={(e) => {
-                    alert("Please complete the add comment section");
-                  }}
-                >
-                  {/* <Typography variant="h4" color="textPrimary">
-                Add Comment
-              </Typography> */}
-                  <TextField
-                    label="Add your comment"
-                    variant="outlined"
-                    fullWidth
-                    multiline
-                    rows={4}
-                    colums={8}
-                    sx={{ borderRadius: "5px", backgroundColor: "rgba(200, 200, 200, 0.4)" }}
-                  />
-                  <Button
-                    variant="contained"
-                    color="primary"
-                    endIcon={<AddComment />}
-                    type="submit"
-                    sx={{ marginTop: 2 }}
-                  >
-                    submit
-                  </Button>
-                </form>
-              </Box>
-            </Box>
-          </Stack>
-        </Stack>
+              {/* Abul said */}
+            </Stack>
+          </Grid>
+        </Grid>
       )}
-    </Box>
+    </>
   );
 };
 
 export default VideoWatch;
+
+// {/* <Stack sx={{ margin: 5 }}>
+//             <Box sx={{ bgcolor: "rgba(160, 111, 205, 0.7)" }}>
+//               <Typography variant="h4" color="textPrimary" sx={{ padding: 2 }}>
+//                 Comments
+//               </Typography>
+//               <Grid container spacing={3} sx={{ padding: 2 }}>
+//                 {comments.map((comment) => (
+//                   <Grid item xs={12}>
+//                     <Card>
+//                       <CardHeader
+//                         title={comment.commenter}
+//                         subheader={comment.comment}
+//                       />
+//                     </Card>
+//                   </Grid>
+//                 ))}
+//               </Grid>
+//               <Box sx={{ padding: 2 }}>
+//                 <form
+//                   onSubmit={(e) => {
+//                     alert("Please complete the add comment section");
+//                   }}
+//                 >
+//                   {/* <Typography variant="h4" color="textPrimary">
+//                 Add Comment
+//               </Typography> */}
+//                   <TextField
+//                     label="Add your comment"
+//                     variant="outlined"
+//                     fullWidth
+//                     multiline
+//                     rows={4}
+//                     colums={8}
+//                     sx={{
+//                       borderRadius: "5px",
+//                       backgroundColor: "rgba(200, 200, 200, 0.4)",
+//                     }}
+//                   />
+//                   <Button
+//                     variant="contained"
+//                     color="primary"
+//                     endIcon={<AddComment />}
+//                     type="submit"
+//                     sx={{ marginTop: 2 }}
+//                   >
+//                     submit
+//                   </Button>
+//                 </form>
+//               </Box>
+//             </Box>
+//           </Stack> */}
