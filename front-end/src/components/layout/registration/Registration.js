f82be9961c804da66e51b4c279ad70aad5b1389a
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-24 22:04:13 +0600

    almost registration

--- front-end/src/components/layout/registration/Registration.js
+++ front-end/src/components/layout/registration/Registration.js
@@ -12,68 +12,78 @@ import { Button, TextField } from "@mui/material";
 import "./Registraion.css";
 
 const Registration = () => {
-  const navigate = useNavigate();
-  const [state, dispatch] = useLoginContext();
-  const formik = useFormik({
-    initialValues: {
-      userName: "",
-      firstName: "",
-      lastName: "",
-      email: "",
-      password: "",
-      confirmPassword: "",
-      picked: STUDENT,
-    },
-    validationSchema: Yup.object({
-      userName: Yup.string().required("User name is required"),
-      firstName: Yup.string()
-        .max(15, "Must be 15 characters or less")
-        .required("Required"),
-      lastName: Yup.string()
-        .max(20, "Must be 20 characters or less")
-        .required("Required"),
-      email: Yup.string().email("Invalid email address").required("Required"),
-      password: Yup.string().min(3, "Password is weak").required("Required"),
-      confirmPassword: Yup.string()
-        .required("Please confirm your password")
-        .oneOf([Yup.ref("password")], "Passwords do not match"),
-    }),
-    onSubmit: (values) => {
-      // alert(JSON.stringify(values, null, 2));
-      console.log(values);
-      RegistrationService.register(
-        values.userName,
-        values.firstName,
-        values.lastName,
-        values.email,
-        values.password,
-        values.picked
-      )
-        .then((res) => {
-          alert("account was created successfully");
-          console.log(res);
-          console.error("redirect to login with homepage");
-          LoginService.LoginWithDispatch(
-            values.userName,
-            values.password,
-            dispatch
-          );
-          navigate("/");
-        })
-        .catch((err) => {
-          alert(err.message);
-        })
-        .finally(() => {});
-    },
-  });
-  return (
-    <Paper className="gradient-custom" sx={{ padding: "32px" }} elevation={2}>
-      <h3 className="mb-4 pb-2 pb-md-0 mb-md-5">Let's start our journey!</h3>
-      <form onSubmit={formik.handleSubmit}>
-        <Stack container spacing={4}>
-          {/*<div className="row">*/}
-          {/*<div className="col-md-6 mb-4">*/}
-          {/* <div className="form-outline">
+	const navigate = useNavigate();
+	const [state, dispatch] = useLoginContext();
+	const formik = useFormik({
+		initialValues: {
+			userName: "",
+			firstName: "",
+			lastName: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+			picked: STUDENT,
+		},
+		validationSchema: Yup.object({
+			userName: Yup.string().required("User name is required"),
+			firstName: Yup.string()
+				.max(15, "Must be 15 characters or less")
+				.required("Required"),
+			lastName: Yup.string()
+				.max(20, "Must be 20 characters or less")
+				.required("Required"),
+			email: Yup.string()
+				.email("Invalid email address")
+				.required("Required"),
+			password: Yup.string()
+				.min(3, "Password is weak")
+				.required("Required"),
+			confirmPassword: Yup.string()
+				.required("Please confirm your password")
+				.oneOf([Yup.ref("password")], "Passwords do not match"),
+		}),
+		onSubmit: (values) => {
+			// alert(JSON.stringify(values, null, 2));
+			console.log(values);
+			RegistrationService.register(
+				values.userName,
+				values.firstName,
+				values.lastName,
+				values.email,
+				values.password,
+				values.picked
+			)
+				.then((res) => {
+					alert("account was created successfully");
+					console.log(res);
+					console.error("redirect to login with homepage");
+					LoginService.LoginWithDispatch(
+						values.userName,
+						values.password,
+						dispatch
+					);
+					navigate("/");
+				})
+				.catch((err) => {
+					alert(err.message);
+				})
+				.finally(() => {});
+		},
+	});
+	return (
+		<Paper
+			className="gradient-custom"
+			sx={{ padding: "32px" }}
+			elevation={2}
+		>
+			<h3 className="mb-4 pb-2 pb-md-0 mb-md-5">
+				Let's start our journey!
+			</h3>
+			<form onSubmit={formik.handleSubmit}>
+				<Stack container spacing={4}>
+					{/*<div className="row">*/}
+					{/*<div className="col-md-6 mb-4">*/}
+					{/* <div className="form-outline">
                                         <label className="form-label" htmlFor="userName">User Name</label>
 
                                         <input
@@ -88,128 +98,157 @@ const Registration = () => {
                                         {formik.touched.userName && formik.errors.userName ? (
                                             <div>{formik.errors.userName}</div>
                                         ) : null} */}
-          {/*</div>*/}
-          {/*</div>*/}
-          {/* </div> */}
-          <Stack>
-            <TextField
-              label={"User Name"}
-              id={"userName"}
-              type={"text"}
-              variant={"outlined"}
-              value={formik.values.userName}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={formik.touched.userName && Boolean(formik.errors.userName)}
-              helperText={formik.touched.userName && formik.errors.userName}
-              required
-            />
+					{/*</div>*/}
+					{/*</div>*/}
+					{/* </div> */}
+					<Stack>
+						<TextField
+							label={"User Name"}
+							id={"userName"}
+							type={"text"}
+							variant={"outlined"}
+							value={formik.values.userName}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.userName &&
+								Boolean(formik.errors.userName)
+							}
+							helperText={
+								formik.touched.userName &&
+								formik.errors.userName
+							}
+							required
+						/>
 
-            <TextField
-              label={"First Name"}
-              id={"firstName"}
-              type={"text"}
-              variant={"outlined"}
-              value={formik.values.firstName}
-              onChange={formik.handleChange}
-              onBlur={formik.handleBlur}
-              error={
-                formik.touched.firstName && Boolean(formik.errors.firstName)
-              }
-              helperText={formik.touched.firstName && formik.errors.firstName}
-              required
-            />
-          </Stack>
+						<TextField
+							label={"First Name"}
+							id={"firstName"}
+							type={"text"}
+							variant={"outlined"}
+							value={formik.values.firstName}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.firstName &&
+								Boolean(formik.errors.firstName)
+							}
+							helperText={
+								formik.touched.firstName &&
+								formik.errors.firstName
+							}
+							required
+						/>
 
-          <label htmlFor="lastName">Last Name</label>
-          <input
-            id="lastName"
-            name="lastName"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.lastName}
-          />
-          {formik.touched.lastName && formik.errors.lastName ? (
-            <div>{formik.errors.lastName}</div>
-          ) : null}
+						<TextField
+							label={"Last Name"}
+							id={"lastName"}
+							type={"text"}
+							variant={"outlined"}
+							value={formik.values.lastName}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.lastName &&
+								Boolean(formik.errors.lastName)
+							}
+							helperText={
+								formik.touched.lastName &&
+								formik.errors.lastName
+							}
+							required
+						/>
 
-          <label htmlFor="email">Email Address</label>
-          <input
-            id="email"
-            name="email"
-            type="text"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          {formik.touched.email && formik.errors.email ? (
-            <div>{formik.errors.email}</div>
-          ) : null}
+						<TextField
+							label={"Email"}
+							id={"email"}
+							type={"email"}
+							variant={"outlined"}
+							value={formik.values.email}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.email &&
+								Boolean(formik.errors.email)
+							}
+							helperText={
+								formik.touched.email && formik.errors.email
+							}
+							required
+						/>
 
-          {/* <label htmlFor='password'>Password</label>
-            <input
-                id = 'password' */}
+						<TextField
+							label={"Password"}
+							id={"password"}
+							type={"password"}
+							variant={"outlined"}
+							value={formik.values.password}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.password &&
+								Boolean(formik.errors.password)
+							}
+							helperText={
+								formik.touched.password &&
+								formik.errors.password
+							}
+							required
+						/>
 
-          <label htmlFor="password">Password </label>
-          <input
-            id="password"
-            name="password"
-            type="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
-          {formik.touched.password && formik.errors.password ? (
-            <div>{formik.errors.password}</div>
-          ) : null}
+						<TextField
+							label={"Confirm Password"}
+							id={"confirmPassword"}
+							type={"password"}
+							variant={"outlined"}
+							value={formik.values.confirmPassword}
+							onChange={formik.handleChange}
+							onBlur={formik.handleBlur}
+							error={
+								formik.touched.confirmPassword &&
+								Boolean(formik.errors.confirmPassword)
+							}
+							helperText={
+								formik.touched.confirmPassword &&
+								formik.errors.confirmPassword
+							}
+							required
+						/>
+					</Stack>
 
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input
-            id="confirmPassword"
-            name="confirmPassword"
-            type="password"
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.confirmPassword}
-          />
-          {formik.touched.confirmPassword && formik.errors.confirmPassword ? (
-            <div>{formik.errors.confirmPassword}</div>
-          ) : null}
+					<div id="my-radio-group">Role</div>
+					<div role="group" aria-labelledby="my-radio-group">
+						<label>
+							<input
+								type="radio"
+								name="picked"
+								value={STUDENT}
+								onClick={formik.handleChange}
+							/>
+							student
+						</label>
+						<label>
+							<input
+								type="radio"
+								name="picked"
+								value={INSTRUCTOR}
+								onClick={formik.handleChange}
+							/>
+							instructor
+						</label>
+					</div>
 
-          <div id="my-radio-group">Role</div>
-          <div role="group" aria-labelledby="my-radio-group">
-            <label>
-              <input
-                type="radio"
-                name="picked"
-                value={STUDENT}
-                onClick={formik.handleChange}
-              />
-              student
-            </label>
-            <label>
-              <input
-                type="radio"
-                name="picked"
-                value={INSTRUCTOR}
-                onClick={formik.handleChange}
-              />
-              instructor
-            </label>
-          </div>
+					<Grid justify="center">
+						<Button variant={"contained"} type={"submit"}>
+							Register
+						</Button>
+					</Grid>
 
-          <Grid justify="center">
-            <Button variant={"contained"} type={"submit"}>
-              Register
-            </Button>
-          </Grid>
-
-          {/*</Container>*/}
-        </Stack>
-      </form>
-    </Paper>
-  );
+					{/*</Container>*/}
+				</Stack>
+			</form>
+		</Paper>
+	);
 };
 
 export default Registration;
