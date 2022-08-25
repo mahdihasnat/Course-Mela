5bd7e469b3a5074149c50285218ca656d0b6c7aa
vodro <vdrkowshic@gmail.com>
2022-08-25 19:53:27 +0600

    video log saved

--- front-end/src/components/views/shared/videoWatch/VideoWatch.js
+++ front-end/src/components/views/shared/videoWatch/VideoWatch.js
@@ -1,107 +1,146 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {useParams} from "react-router-dom";
 import {
-  Avatar,
-  Box,
-  Button,
-  Card,
-  CardHeader,
-  CircularProgress,
-  Container,
-  Grid,
-  Paper,
-  Stack,
-  TextField,
-  Typography,
+    Avatar,
+    Box,
+    Button,
+    Card,
+    CardHeader,
+    CircularProgress,
+    Container,
+    Grid,
+    Paper,
+    Stack,
+    TextField,
+    Typography,
 } from "@mui/material";
 
 
 import ReactPlayer from "react-player";
 import VideoService from "../../../../services/video/VideoService";
-import { LOG_CAUGHT_ERR } from "../../../../shared/utils";
+import {LOG_CAUGHT_ERR} from "../../../../shared/utils";
 import Playlist from "./Playlist";
-import Avaatar, { getAvatar } from "../../../../utils/Avatar";
-import { CommentCard } from "../../../helper/comment/CommentCard";
+import Avaatar, {getAvatar} from "../../../../utils/Avatar";
+import {CommentCard} from "../../../helper/comment/CommentCard";
 import CommentService from "../../../../services/comment/CommentService";
-import { VideoCardHorizontal } from "../../../helper/VideoCard";
-import { AddComment } from "@mui/icons-material";
+import {VideoCardHorizontal} from "../../../helper/VideoCard";
+import {AddComment} from "@mui/icons-material";
 import AddCommentInput from "../../../helper/comment/AddCommentInput";
 import {CommentSection} from "../../../helper/comment/CommentSection";
+import getCurrentDateTime from "../../../../utils/Time";
+
+
+const CustomPlayer = ({video}) => {
+    // const [last, setLast] = useState(initState);
+    const [lastRecorededTime, setLastRecorededTime] = useState(0);
+    const [visitTime, setVisitTime] = useState(getCurrentDateTime());
+    const progressCallback = ({played, playedSeconds,loaded, loadedSeconds}) => {
+        // alert("5 sec por por eta aseh kora hoye");
+        console.log({played, playedSeconds,loaded, loadedSeconds});
+        const watchTime = playedSeconds - lastRecorededTime;
+        if(watchTime > 0){
+            VideoService.updateWatchTime(video.id, watchTime, playedSeconds, visitTime).then(
+                (response) => {
+                    console.log({response});
+                }
+            )
+                .catch(LOG_CAUGHT_ERR);
+        }
+        setLastRecorededTime(playedSeconds);
+
+    }
+    const onReadyCallback = () => {
+        console.log("onReadyCallback");
+        setVisitTime(getCurrentDateTime());
+    }
+    const seekCallback = (second) =>{
+        console.log({"seeked to ":second});
+        setLastRecorededTime(second);
+    }
+
+    return (<ReactPlayer
+            width={"100%"}
+            height={"100%"}
+            url={video.videoPath}
+            controls
+            progressInterval={5000}
+            onProgress={progressCallback}
+            light={video.thumbPath}
+            onReady={onReadyCallback}
+            onSeek={seekCallback}
+
+        ></ReactPlayer>
+    )
+}
 
 const VideoWatch = ({}) => {
-  const { videoId } = useParams();
+    const {videoId} = useParams();
 
-  const [playlists, setPlaylists] = useState([]);
+    const [playlists, setPlaylists] = useState([]);
 
-  const [video, setvideo] = useState(null);
+    const [video, setvideo] = useState(null);
 
-  const [isLoading, setIsLoading] = useState(true);
+    const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    /// loading the videos here
+    useEffect(() => {
+        /// loading the videos here
 
-    VideoService.getVideoById(videoId)
-      .then((response) => {
-        console.log({ video: response.data });
-        setvideo(response.data);
-        setIsLoading(false);
-      })
-      .catch(LOG_CAUGHT_ERR);
-    /// similar videos will be loaded here
+        VideoService.getVideoById(videoId)
+            .then((response) => {
+                console.log({video: response.data});
+                setvideo(response.data);
+                setIsLoading(false);
+            })
+            .catch(LOG_CAUGHT_ERR);
+        /// similar videos will be loaded here
 
-    VideoService.getSimilarVideos(videoId)
-      .then((response) => {
-        console.log({
-          currentVideos: videoId,
-          similarVideos: response.data,
-        });
+        VideoService.getSimilarVideos(videoId)
+            .then((response) => {
+                console.log({
+                    currentVideos: videoId,
+                    similarVideos: response.data,
+                });
 
-        setPlaylists(response.data);
-      })
-      .catch(LOG_CAUGHT_ERR);
-  }, [videoId]);
-
-
+                setPlaylists(response.data);
+            })
+            .catch(LOG_CAUGHT_ERR);
+    }, [videoId]);
 
 
+    return (
+        <>
+            {isLoading ? (
+                <CircularProgress height="100%"/>
+            ) : (
+                <Grid container my={5}>
+                    <Grid item sm={12} md={8} px={5}>
 
+                        {/*//// PLayer*/}
 
-  return (
-    <>
-      {isLoading ? (
-        <CircularProgress height="100%" />
-      ) : (
-        <Grid container my={5}>
-          <Grid item sm={12} md={8} px={5}>
-            <ReactPlayer
-              width={"100%"}
-              height={"100%"}
-              url={video.videoPath}
-              controls
-            ></ReactPlayer>
-          </Grid>
-          {/* TODO: simplify this and add scrollbar */}
-          <Grid item sm={12} md={3} marginLeft={10}>
-            <Paper style={{ overflow: "auto" }}>
-              <Stack spacing={2}>
-                {/* <Box sx={{ height: 700, width: 450, overflow: "auto" }}>
+                        <CustomPlayer video={video}/>
+                    </Grid>
+                    {/* TODO: simplify this and add scrollbar */}
+                    <Grid item sm={12} md={3} marginLeft={10}>
+                        <Paper style={{overflow: "auto"}}>
+                            <Stack spacing={2}>
+                                {/* <Box sx={{ height: 700, width: 450, overflow: "auto" }}>
                 { */}
-                {
-                  /// TODO : check if it actually works playlists &&
-                  playlists.map((playlist) => (
-                    <VideoCardHorizontal key={playlist.id} {...playlist} />
-                  ))
-                }
-              </Stack>
-            </Paper>
-          </Grid>
-          <Grid item sm={12} md={8}>
-              <CommentSection videoId={videoId}/>
-          </Grid>
-        </Grid>
-      )}
-    </>
-  );
+                                {
+                                    /// TODO : check if it actually works playlists &&
+                                    playlists.map((playlist) => (
+                                        <VideoCardHorizontal key={playlist.id} {...playlist} />
+                                    ))
+                                }
+                            </Stack>
+                        </Paper>
+                    </Grid>
+                    <Grid item sm={12} md={8}>
+                        <CommentSection videoId={videoId}/>
+                    </Grid>
+                </Grid>
+            )}
+        </>
+    );
 };
 
 export default VideoWatch;
---
+++ front-end/src/utils/Time.js
@@ -0,0 +1,15 @@
+// var today = new Date(),
+//
+//     time = today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+
+const getCurrentDateTime =() => {
+    const today = new Date();
+    // console.log(today.toISOString())
+    return today.toISOString();
+}
+
+export default getCurrentDateTime;
+
+
+// getCurrentDateTime();
