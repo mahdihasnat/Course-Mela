cc9c2b2107456a4b9bfeb2f9766097c79c2e1e70
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-26 18:25:07 +0600

    fixed comment issue

--- front-end/src/components/helper/comment/AddCommentInput.js
+++ front-end/src/components/helper/comment/AddCommentInput.js
@@ -1,71 +1,70 @@
-import {Box, Button, Stack, TextField} from "@mui/material";
-import {AddComment} from "@mui/icons-material";
+import { Box, Button, Stack, TextField } from "@mui/material";
+import { AddComment } from "@mui/icons-material";
 import React from "react";
 import CommentService from "../../../services/comment/CommentService";
-import {LOG_CAUGHT_ERR} from "../../../shared/utils";
+import { LOG_CAUGHT_ERR } from "../../../shared/utils";
 
+const AddCommentInput = ({ videoId, setCommentsUpdated }) => {
+	const [comment, setComment] = React.useState("");
 
-const AddCommentInput = ({videoId, setCommentsUpdated}) => {
-    const [comment, setComment] = React.useState('');
+	const updateComment = (e) => {
+		setComment(e.target.value);
+		console.log(comment);
+	};
 
-    const updateComment = (e) => {
-        setComment(e.target.value);
-        console.log(comment);
-    }
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		if (comment.length == 0) return;
+		console.log({ comment });
+		CommentService.addComment(videoId, comment)
+			.then((response) => {
+				// alert("Comment added successfully");
+				console.log({ "Doubt submitted ": response });
+				setComment("");
 
+				setCommentsUpdated(true);
+				// now update the comments
+			})
+			.catch(LOG_CAUGHT_ERR);
+	};
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
-        console.log({comment})
-        CommentService.addComment(videoId, comment).then(
-            (response) => {
-                // alert("Comment added successfully");
-                console.log({"Doubt submitted ": response});
-                setComment('');
-
-                setCommentsUpdated(true);
-                // now update the comments
-            }
-        ).catch(LOG_CAUGHT_ERR);
-    }
-
-    return (<Box padding={2}>
-        <form
-            onSubmit={handleSubmit}
-        >
-
-            <TextField
-                label="Ask your doubts"
-                variant="outlined"
-                fullWidth
-                multiline
-                rows={4}
-                colums={8}
-                sx={{
-                    borderRadius: "5px", backgroundColor: "rgba(200, 200, 200, 0.4)",
-                }}
-                value={comment}
-                onChange={updateComment}
-            />
-            <Box display="flex"
-                 justifyContent="flex-start"
-                 alignItems="flex-start"
-            >
-                <Button
-                    variant="contained"
-                    color="primary"
-                    endIcon={<AddComment/>}
-                    type="submit"
-                    sx={{marginTop: 2}}
-                >
-                    submit
-                </Button>
-            </Box>
-        </form>
-    </Box>)
-}
-
+	return (
+		<Box padding={2}>
+			<form onSubmit={handleSubmit}>
+				<TextField
+					label="Ask your doubts"
+					variant="outlined"
+					required
+					fullWidth
+					multiline
+					rows={4}
+					colums={8}
+					sx={{
+						borderRadius: "5px",
+						backgroundColor: "rgba(200, 200, 200, 0.4)",
+					}}
+					value={comment}
+					onChange={updateComment}
+				/>
+				<Box
+					display="flex"
+					justifyContent="flex-start"
+					alignItems="flex-start"
+				>
+					<Button
+						variant="contained"
+						color="primary"
+						endIcon={<AddComment />}
+						type="submit"
+						sx={{ marginTop: 2 }}
+					>
+						submit
+					</Button>
+				</Box>
+			</form>
+		</Box>
+	);
+};
 
 export default AddCommentInput;
 
--- front-end/src/components/helper/comment/CommentCard.js
+++ front-end/src/components/helper/comment/CommentCard.js
@@ -1,101 +1,135 @@
-import {Box, Button, Grid, Stack, TextField, Typography} from "@mui/material";
+import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import React from "react";
 import Avaatar from "../../../utils/Avatar";
-import {ExpandLessRounded, ExpandMoreRounded} from "@material-ui/icons";
-import SendIcon from '@mui/icons-material/Send';
-import CancelIcon from '@mui/icons-material/Cancel';
+import { ExpandLessRounded, ExpandMoreRounded } from "@material-ui/icons";
+import SendIcon from "@mui/icons-material/Send";
+import CancelIcon from "@mui/icons-material/Cancel";
 import CommentService from "../../../services/comment/CommentService";
 
-
-
-
-
 export const CommentCard = ({
-                                videoId,   id, text, postTime, clarificationStatus, userId, userName, replies, setCommentsUpdated
-                            }) => {
+	videoId,
+	id,
+	text,
+	postTime,
+	clarificationStatus,
+	userId,
+	userName,
+	replies,
+	setCommentsUpdated,
+}) => {
+	const [isExpanded, setIsExpanded] = React.useState(true);
+	const [isReplyExpanded, setIsReplyExpanded] = React.useState(false);
+	const [replyText, setReplyText] = React.useState("");
+	// const [newReplies, setNewReplies] = React.useState([]);
 
-    const [isExpanded, setIsExpanded] = React.useState(true);
-    const [isReplyExpanded, setIsReplyExpanded] = React.useState(false);
-    const [replyText, setReplyText] = React.useState("");
-    // const [newReplies, setNewReplies] = React.useState([]);
+	const submitClarification = (e) => {
+		e.preventDefault();
+		if (replyText.length == 0) return;
+		console.log({ submitClarification: replyText });
+		CommentService.addClarification(videoId, id, replyText).then(
+			(response) => {
+				console.log({ "submitClarification Result": response.data });
+				setReplyText("");
+				setIsReplyExpanded(false);
+				setCommentsUpdated(true);
+			}
+		);
+	};
 
-    const submitClarification = (e)=>{
-        e.preventDefault();
-        console.log({"submitClarification": replyText});
-        CommentService.addClarification(videoId, id, replyText).then(
-            (response)=>{
-                console.log({"submitClarification Result": response.data});
-                setReplyText("");
-                setIsReplyExpanded(false);
-                setCommentsUpdated(true);
-            }
-        )
-    }
+	return (
+		<Box my={2}>
+			<Box border={0} padding={0}>
+				<Grid container spacing={1}>
+					<Grid item sm={2}>
+						<Avaatar name={userName} />
+					</Grid>
+					<Grid item sm={9}>
+						<Typography variant={"h6"} gutterBottom>
+							{userName}
+						</Typography>
+						<Typography
+							variant="body1"
+							align="justify"
+							gutterBottom
+						>
+							{text}
+						</Typography>
+						<Stack spacing={1} direction={"row"} marginTop={3}>
+							{!isReplyExpanded && (
+								<Button
+									variant="outlined"
+									color="primary"
+									onClick={(e) => setIsReplyExpanded(true)}
+								>
+									{" "}
+									Reply{" "}
+								</Button>
+							)}
+							{isReplyExpanded && (
+								<Stack direction={"row"}>
+									<TextField
+										label={"Help solving other's doubts"}
+										required
+										value={replyText}
+										onChange={(e) =>
+											setReplyText(e.target.value)
+										}
+									/>
 
-    return (
-        <Box my={2}>
-        <Box border={0}  padding={0}>
-        <Grid container spacing={1}>
-            <Grid item sm={2}>
-                <Avaatar name={userName}/>
+									{/*</TextField>*/}
+									<Button
+										endIcon={<SendIcon />}
+										onClick={submitClarification}
+									></Button>
+									<Button
+										startIcon={<CancelIcon />}
+										onClick={(e) =>
+											setIsReplyExpanded(false)
+										}
+									></Button>
+								</Stack>
+							)}
+						</Stack>
 
-            </Grid>
-            <Grid item sm={9}>
-                <Typography variant={"h6"} gutterBottom>{userName}</Typography>
-                <Typography variant="body1" align="justify" gutterBottom>
-                    {text}
-                </Typography>
-                <Stack spacing={1} direction={"row"} marginTop={3}>
-                    {!isReplyExpanded && <Button variant="outlined" color="primary"
-                                                 onClick={e => setIsReplyExpanded(true)}> Reply </Button>
+						{/*{*/}
 
+						{/*    isExpanded && replies && replies.map((reply) => (<CommentCard {...reply}/>))*/}
 
-                    }
-                    {isReplyExpanded && <Stack direction={"row"}>
-                        <TextField
-                        label={'Help solving other\'s doubts'}
-                        value={replyText}
-                        onChange={e => setReplyText(e.target.value)}
-
-                        />
-
-                        {/*</TextField>*/}
-                        <Button endIcon={<SendIcon/>}
-                            onClick={submitClarification}
-                        ></Button>
-                        <Button startIcon={<CancelIcon/>}
-                                onClick={e => setIsReplyExpanded(false)}
-                        ></Button>
-                    </Stack>
-
-                    }
-                </Stack>
-
-                {/*{*/}
-
-                {/*    isExpanded && replies && replies.map((reply) => (<CommentCard {...reply}/>))*/}
-
-                {/*}*/}
-            </Grid>
-            <Grid item sm={1}>
-                <Button variant="text" color="primary"
-                        startIcon={isExpanded ? <ExpandLessRounded/> : <ExpandMoreRounded/> }
-                        onClick={() => setIsExpanded(!isExpanded)}
-
-                ></Button>
-            </Grid>
-        </Grid>
-    </Box>
-    <Grid container>
-        <Grid item sm={2}>
-        </Grid>
-        <Grid item sm={10}>
-            {
-                isExpanded && replies && replies.map((reply) => (
-                    reply.clarificationStatus && <CommentCard  setCommentsUpdated={setCommentsUpdated} videoId={videoId} {...reply}/>
-                ))
-            }
-        </Grid>
-    </Grid>
-        </Box>);
+						{/*}*/}
+					</Grid>
+					<Grid item sm={1}>
+						<Button
+							variant="text"
+							color="primary"
+							startIcon={
+								isExpanded ? (
+									<ExpandLessRounded />
+								) : (
+									<ExpandMoreRounded />
+								)
+							}
+							onClick={() => setIsExpanded(!isExpanded)}
+						></Button>
+					</Grid>
+				</Grid>
+			</Box>
+			<Grid container>
+				<Grid item sm={2}></Grid>
+				<Grid item sm={10}>
+					{isExpanded &&
+						replies &&
+						replies.map(
+							(reply) =>
+								reply.clarificationStatus && (
+									<CommentCard
+										setCommentsUpdated={setCommentsUpdated}
+										videoId={videoId}
+										{...reply}
+									/>
+								)
+						)}
+				</Grid>
+			</Grid>
+		</Box>
+	);
 };
