fd669975b3d6f2476e86a84f4b66997b62343c39
Mahdi Hasnat Siyam <mahdibuet3@gmail.com>
2022-08-26 23:09:04 +0600

    added recommendation controller

---
+++ back-end/src/main/java/io/coursemela/coursemela/course/controller/RecommendationController.java
@@ -0,0 +1,31 @@
+package io.coursemela.coursemela.course.controller;
+
+import io.coursemela.coursemela.course.service.CourseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/course/popular")
+public class RecommendationController {
+
+    @Autowired
+    CourseService courseService;
+
+    @GetMapping("watchTime")
+    ResponseEntity getByWatchTime() {
+        return ResponseEntity.ok(courseService.getCourses());
+    }
+
+    @GetMapping("sale")
+    ResponseEntity getBySale() {
+        return ResponseEntity.ok(courseService.getCourses());
+    }
+
+    @GetMapping("recent")
+    ResponseEntity getByRecent() {
+        return ResponseEntity.ok(courseService.getCourses());
+    }
+}
