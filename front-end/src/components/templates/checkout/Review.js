d37271e345c2e5228e373e42b3e7621b32c235ef
vodro <vdrkowshic@gmail.com>
2022-08-26 21:05:46 +0600

    payment info added

--- front-end/src/components/templates/checkout/Review.js
+++ front-end/src/components/templates/checkout/Review.js
@@ -1,30 +1,34 @@
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import Grid from '@mui/material/Grid';
-import { useSelectedCourseContext } from '../../../store/contexts/SelectedCourseContext';
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Grid from "@mui/material/Grid";
+import { useSelectedCourseContext } from "../../../store/contexts/SelectedCourseContext";
 import {
-    getPayableAmountForACourse,
-    getTotalAmountForAllCourses,
-    getTotalAmountWithPromo
-} from '../../../utils/coursePricing';
-import { Box } from '@mui/material';
-import { TakaSign } from '../../helper/CustomIcons';
+  getPayableAmountForACourse,
+  getTotalAmountForAllCourses,
+  getTotalAmountWithPromo,
+} from "../../../utils/coursePricing";
+import { Box } from "@mui/material";
+import { TakaSign } from "../../helper/CustomIcons";
+import { useLoginContext } from "../../../store/contexts/LoginContext";
 
-
-
-const addresses = ['1 MUI Drive', 'Reactville', 'Anytown', '99999', 'USA'];
-const payments = [
-  { name: 'Card type', detail: 'Visa' },
-  { name: 'Card holder', detail: 'Mr John Smith' },
-  { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
-  { name: 'Expiry date', detail: '04/2024' },
-];
+const addresses = ["1 MUI Drive", "Reactville", "Anytown", "99999", "USA"];
+// const payments = [
+//   { name: 'Card type', detail: 'Visa' },
+//   { name: 'Card holder', detail: 'Mr John Smith' },
+//   { name: 'Card number', detail: 'xxxx-xxxx-xxxx-1234' },
+//   { name: 'Expiry date', detail: '04/2024' },
+// ];
 
 export default function Review() {
-  const [{cartCourses, selectedPromo}, dispatch] = useSelectedCourseContext();
+  const [
+    { cartCourses, selectedPromo, paymentType, paymentAccountNo },
+    dispatch,
+  ] = useSelectedCourseContext();
+
+  const [{ userName }] = useLoginContext();
 
   return (
     <React.Fragment>
@@ -35,17 +39,19 @@ export default function Review() {
         {cartCourses.map((course) => (
           <ListItem key={course.name} sx={{ py: 1, px: 0 }}>
             <ListItemText primary={course.name} />
-            <Typography variant="body2">{getPayableAmountForACourse(course)} <TakaSign/></Typography>
+            <Typography variant="body2">
+              {getPayableAmountForACourse(course)} <TakaSign />
+            </Typography>
           </ListItem>
         ))}
-        <Box sx={{ borderTop: '1px solid rgba(0, 0, 0, 0.12)', px: 3 }} marginBottom={4}/>
+        <Box
+          sx={{ borderTop: "1px solid rgba(0, 0, 0, 0.12)", px: 3 }}
+          marginBottom={4}
+        />
         <ListItem sx={{ py: 1, px: 0 }}>
           <ListItemText primary="Total" />
-          <Typography variant="subtitle1" sx={{ fontWeight: 700 }} >
-            {getTotalAmountWithPromo(
-                cartCourses,
-                selectedPromo
-            )} <TakaSign/>
+          <Typography variant="subtitle1" sx={{ fontWeight: 700 }}>
+            {getTotalAmountWithPromo(cartCourses, selectedPromo)} <TakaSign />
           </Typography>
         </ListItem>
       </List>
@@ -62,16 +68,33 @@ export default function Review() {
             Payment details
           </Typography>
           <Grid container>
-            {payments.map((payment) => (
-              <React.Fragment key={payment.name}>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.name}</Typography>
-                </Grid>
-                <Grid item xs={6}>
-                  <Typography gutterBottom>{payment.detail}</Typography>
-                </Grid>
-              </React.Fragment>
-            ))}
+            {/* {payments.map((payment) => ( */}
+            <React.Fragment>
+              <Grid item xs={6}>
+                <Typography gutterBottom>Type</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography gutterBottom>{paymentType}</Typography>
+              </Grid>
+            </React.Fragment>
+            <React.Fragment>
+              <Grid item xs={6}>
+                <Typography gutterBottom>User Name</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography gutterBottom>{userName}</Typography>
+              </Grid>
+            </React.Fragment>
+
+            <React.Fragment>
+              <Grid item xs={6}>
+                <Typography gutterBottom>Account No</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Typography gutterBottom>{paymentAccountNo}</Typography>
+              </Grid>
+            </React.Fragment>
+            {/* ))} */}
           </Grid>
         </Grid>
       </Grid>
