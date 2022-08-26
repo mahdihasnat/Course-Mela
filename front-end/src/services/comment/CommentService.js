83b301c5e096af1d80382b19f10c6dafb551e58c
vodro <vdrkowshic@gmail.com>
2022-08-26 19:16:42 +0600

    approve and delete

--- front-end/src/services/comment/CommentService.js
+++ front-end/src/services/comment/CommentService.js
@@ -4,43 +4,47 @@ import { COMMENT_URL } from "../../shared/urls";
 import joinUrl from "../../utils/url";
 
 class CommentService {
-	fetchAllComments(videoId) {
-		return axios.get(joinUrl(COMMENT_URL, "video", videoId), {
-			headers: jsonAuthorizedHeader(),
-		});
-	}
+  fetchAllComments(videoId) {
+    return axios.get(joinUrl(COMMENT_URL, "video", videoId), {
+      headers: jsonAuthorizedHeader(),
+    });
+  }
 
-	addComment(videoId, comment) {
-		console.log({"inside comment service": comment});
-		console.log(comment);
-		return axios.post(joinUrl(COMMENT_URL, "video", videoId, "add"),comment, {
-			headers: jsonAuthorizedHeader(),
-		});
-	}
+  addComment(videoId, comment) {
+    console.log({ "inside comment service": comment });
+    console.log(comment);
+    return axios.post(joinUrl(COMMENT_URL, "video", videoId, "add"), comment, {
+      headers: jsonAuthorizedHeader(),
+    });
+  }
 
-	addClarification(videoId, parentClarificationId, reply) {
-		const data = {
-			videoId: videoId,
-			parentClarificationId: parentClarificationId,
-			text: reply,
-		}
-		console.log({"inside comment service": data});
-		return axios.post(joinUrl(COMMENT_URL, "reply","add"),data, {
-			headers: jsonAuthorizedHeader(),
-		});
-	}
+  addClarification(videoId, parentClarificationId, reply) {
+    const data = {
+      videoId: videoId,
+      parentClarificationId: parentClarificationId,
+      text: reply,
+    };
+    console.log({ "inside comment service": data });
+    return axios.post(joinUrl(COMMENT_URL, "reply", "add"), data, {
+      headers: jsonAuthorizedHeader(),
+    });
+  }
 
-	deleteClarification(videoId, id) {
-		return axios.delete(joinUrl(COMMENT_URL, "delete", id), {
-			headers: jsonAuthorizedHeader(),
-		});
-	}
+  deleteClarification(videoId, id) {
+    return axios.delete(joinUrl(COMMENT_URL, "delete", id), {
+      headers: jsonAuthorizedHeader(),
+    });
+  }
 
-	approveClarification(videoId, id) {
-		return axios.put(joinUrl(COMMENT_URL, "approve", id),{},  {
-			headers: jsonAuthorizedHeader(),
-		});
-	}
+  approveClarification(videoId, id) {
+    return axios.put(
+      joinUrl(COMMENT_URL, "approve", id),
+      {},
+      {
+        headers: jsonAuthorizedHeader(),
+      }
+    );
+  }
 }
 
 export default new CommentService();
